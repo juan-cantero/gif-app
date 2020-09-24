4f373c831413b5d0e58830763c07a6f4c60fcaca
Juan Cantero <42438915+juan-cantero@users.noreply.github.com>
2020-09-23 21:56:22 -0300

    finish

---
+++ documents/precache-manifest.e680c26b3f27d46001c5f265133ac0ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d02afec5379eb2bef7c930e1741cf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ede33ec1dae3905e1f",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "45fb48e538dcfdd892b8",
+    "url": "/static/css/main.3c137924.chunk.css"
+  },
+  {
+    "revision": "83ede33ec1dae3905e1f",
+    "url": "/static/js/2.1496392d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1496392d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fb48e538dcfdd892b8",
+    "url": "/static/js/main.1df03588.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
